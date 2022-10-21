cb53a0c50f732df51922da062d9a360adbfd6e10
s38449 <r.bolgov@kmz.su>
2022-10-21 14:29:50 +0300

    первый релиз программы
    добавлен интерфейс командной строки
    поддерживаются аргументы -p, -v
    -s реализован не полностью, пока переносит номер прибора на график

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,51 @@
+use clap::{Arg, Command};
+use std::error::Error;
+use thermo_model::ThermoModel;
+
 mod thermo_model;
 
-fn main() {}
+fn main() -> Result<(), Box<dyn Error>> {
+    let args = Command::new(env!("CARGO_PKG_NAME"))
+        .version(env!("CARGO_PKG_VERSION"))
+        .author(env!("CARGO_PKG_AUTHORS"))
+        .about(env!("CARGO_PKG_DESCRIPTION"))
+        .arg(
+            Arg::new("predict")
+                .short('p')
+                .long("predict")
+                .value_name("CSV FILE")
+                .required_unless_present("validate"),
+        )
+        .arg(
+            Arg::new("validate")
+                .short('v')
+                .long("validate")
+                .value_name("CSV FILE")
+                .required_unless_present("predict")
+                .conflicts_with("predict"),
+        )
+        .arg(
+            Arg::new("serial_number")
+                .short('s')
+                .long("serial_number")
+                .value_name("SERIAL NUMBER")
+                .required(false),
+        )
+        .get_matches();
+
+    let recalc = args.contains_id("predict");
+
+    let binding = "Something goes wrong".to_string();
+    let path = args
+        .get_one::<String>("predict")
+        .unwrap_or_else(|| args.get_one::<String>("validate").unwrap_or(&binding));
+
+    let binding = String::default();
+    let serial_number = args.get_one::<String>("serial_number").unwrap_or(&binding);
+
+    let model = ThermoModel::from_path(path, recalc)?;
+    model.plot(serial_number)?;
+    model.md()?;
+
+    Ok(())
+}
